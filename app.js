75c648fc667709e1357eff44b22c6806b05979e2
yoluwo <yetunde.oluwo@vogappdevelopers.com>
2022-09-13 13:52:58 -0600

    Reset counter when save button is clicked

--- app.js
+++ app.js
@@ -4,22 +4,27 @@
 * Change the count-el in the html to reflect the new count
 */
 
-
+let count = 0
 let countEl = document.getElementById("count-el")
-let count = 0;
+let saveEl = document.getElementById("save-el")
 
 function increment() {
     count += 1
     countEl.textContent = count;
 }
 
-//Create a funtion save() which logs out the count when it's called
+let reset = function() {
+    count = 0;
+    document.getElementById
+}
 
-//Grab the save-el paragraph and store in a variable called saveEl
-let saveEl = document.getElementById("save-el")
-console.log(count, countEl, saveEl)
-
+/*Create a funtion save() which logs out the count when it's called
+and resets the number of people entered when save button is clicked
+*/
 function save() {
     let countStr =" " + count + " , "
     saveEl.innerText += countStr
+    countEl.textContent = 0;
+    count = 0;
+    console.log(saveEl, countEl)
 }
