fc525a99320e3dd8490d6e57683f0a2e503b661f
yoluwo <yetunde.oluwo@vogappdevelopers.com>
2022-09-13 13:23:22 -0600

    created a save button which saves the count and logs it

--- app.js
+++ app.js
@@ -3,12 +3,23 @@
 * listen for clicks on the increment button
 * Change the count-el in the html to reflect the new count
 */
-let countEl = document.getElementById("count-el")
-console.log(countEl)
 
+
+let countEl = document.getElementById("count-el")
 let count = 0;
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count;
+}
+
+//Create a funtion save() which logs out the count when it's called
+
+//Grab the save-el paragraph and store in a variable called saveEl
+let saveEl = document.getElementById("save-el")
+console.log(count, countEl, saveEl)
+
+function save() {
+    let countStr =" " + count + " , "
+    saveEl.innerText += countStr
 }
