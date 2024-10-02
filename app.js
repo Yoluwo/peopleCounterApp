96032825dba95996cf1318b74e9ee38a6270109b
Yetunde Oluwo <77301913+Yoluwo@users.noreply.github.com>
2024-10-01 21:59:34 -0600

    Added clear function

--- app.js
+++ app.js
@@ -4,27 +4,27 @@
 * Change the count-el in the html to reflect the new count
 */
 
-let count = 0
-let countEl = document.getElementById("count-el")
-let saveEl = document.getElementById("save-el")
+let count = 0;
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 function increment() {
-    count += 1
+    count += 1;
     countEl.textContent = count;
-}
-
-let reset = function() {
-    count = 0;
-    document.getElementById
 }
 
 /*Create a funtion save() which logs out the count when it's called
 and resets the number of people entered when save button is clicked
 */
 function save() {
-    let countStr =" "+ count + " , "
-    saveEl.innerText += countStr
+    let countStr =" "+ count + " , ";
+    saveEl.textContent += countStr;
     countEl.textContent = 0;
     count = 0;
-    console.log(saveEl, countEl)
+}
+
+/* Clear button */
+
+function reset() {
+    location.reload();
 }
